c9e4fac61865e2d862741f263b3c1ab8f27d8d99
forrestmorrison <forrestmorrison@gmail.com>
2022-11-17 07:10:07 -0600

    added useGetTodosQuery to TodoList

--- client/src/features/todos/TodoList.js
+++ client/src/features/todos/TodoList.js
@@ -1,9 +1,18 @@
+import { useGetTodosQuery } from '../api/apiSlice'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faUpload } from '@fortawesome/free-solid-svg-icons'
 import { useState } from "react"
 
 const TodoList = () => {
     const [newTodo, setNewTodo] = useState('')
+
+    const {
+        data: todos,
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    } = useGetTodosQuery()
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -29,6 +38,13 @@ const TodoList = () => {
         </form>
 
     let content;
+    if (isLoading) {
+        content = <p>Loading...</p>
+    }   else if (isSuccess) {
+        content = JSON.stringify(todos)
+    }   else if (isError) {
+        content = <p>{error}</p>
+    }
 
     return (
         <main>
